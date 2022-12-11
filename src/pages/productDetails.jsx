6caf445b1177ae6c491c891c04b7550cb2ed407d
checkiamsiam <issiam02415@gmail.com>
2022-12-11 22:04:47 +0600

    almost complete

--- src/pages/productDetails.jsx
+++ src/pages/productDetails.jsx
@@ -7,37 +7,11 @@ import AcUnitIcon from "@mui/icons-material/AcUnit";
 import { useEffect, useRef, useState } from "react";
 
 const ProductDetails = () => {
-  const [open, setOpen] = useState(false);
-  const descriptionElementRef = useRef(null);
-  useEffect(() => {
-    if (open) {
-      const { current: descriptionElement } = descriptionElementRef;
-      if (descriptionElement !== null) {
-        descriptionElement.focus();
-      }
-    }
-  }, [open]);
+ 
   return (
     <Container maxWidth="xl">
-      <IconButton onClick={() => setOpen(true)}>
-        <AcUnitIcon></AcUnitIcon>
-      </IconButton>
-      <Dialog
-        open={open}
-        onClose={() => setOpen(false)}
-        scroll="body"
-        aria-labelledby="scroll-dialog-title"
-        aria-describedby="scroll-dialog-description"
-      >
-        <DialogTitle id="scroll-dialog-title">Get Alert</DialogTitle>
-        <DialogContent dividers>
-          <DialogContentText id="scroll-dialog-description" ref={descriptionElementRef} tabIndex={-1}></DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button variant="contained" sx={{backgroundColor: "transparent" , color: "black", outline: "1px solid black"}}>Add Alert</Button>
-          <Button onClick={()=>setOpen(false)} variant="text" sx={{backgroundColor:"#CACED2", color: "black" }}>Close</Button>
-        </DialogActions>
-      </Dialog>
+
+      
       <ProductDetailsAnalysisSection />
       <ChartAndAnalysis />
       <ProductAnalysisTable />
